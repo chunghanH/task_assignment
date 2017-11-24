d223148c9377362bf7ad69f4bf9ba052abbbe5c5
TomHsieh <checkthis@HsiehTom-MBP.local>
2017-11-24 22:50:55 +0800

    change bootstrap 3 to 4

--- static/js/ta_app.js
+++ static/js/ta_app.js
@@ -1,11 +1,3 @@
-// function confirm_message(message) {
-//     if (confirm(message)){
-//         return true;
-//     }else{
-//         return false;
-//     }
-// }
-
 $('#task_form').submit(function() {
     var c = confirm("按好繼續?");
     return c;
@@ -15,5 +7,4 @@ $(function(){
     $('#datepicker1').datepicker({
         dateFormat: 'yy-mm-dd'
     });
-    
 });
